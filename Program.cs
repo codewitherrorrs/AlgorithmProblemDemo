9fe517811942e7386aa6828f5c9c56e847aa594d
“codewitherrors” <codewitherrors1@gmail.com>
2023-02-28 21:51:56 +0530

    Add : Ordered List

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Immutable;
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.ComponentModel.DataAnnotations;
 using System.Linq.Expressions;
 using System.Security.Cryptography.X509Certificates;
@@ -9,93 +10,92 @@ namespace AlgorithmProblemDemo
     public class Program
     {
 
-        class Bank
+        Node head; // head of list
+
+        /* Linked list Node*/
+        class Node
         {
-            string name, address;
-            char acc_type;
-            float balance;
-            // Function to open the account
-            public void open_account()
+            public int data;
+            public Node next;
+            public Node(int d)
             {
-                name = "Aman Jhurani";
-                Console.WriteLine("Enter your full name: " + name);
-                address = "Surat";
-                Console.WriteLine("Enter your address: " + address);
-                acc_type = 'S';
-                Console.WriteLine(
-                    "What type of account you want to open saving(S) or Current(C): "
-                    + acc_type);
-                balance = 8000;
-                Console.WriteLine(
-                    "Enter How much money you want to deposit: "
-                    + balance);
-                Console.WriteLine("Account Created Successfully");
-            }
-
-            // Function to deposit the account
-            public void deposit_money()
-            {
-                int Amount = 9500;
-                Console.WriteLine(
-                    "Enter how much money you want to deposit: "
-                    + Amount);
-                balance += Amount;
-                Console.WriteLine("\nAvailable Balance: "
-                                  + balance);
-            }
-
-            // Function to withdraw the account
-            public void withdraw_money()
-            {
-                float amount = 3200;
-                Console.WriteLine(
-                    "Enter how much money you want to withdraw: "
-                    + amount);
-                balance -= amount;
-                Console.WriteLine("\nAvailable balance: "
-                                  + balance);
-            }
-
-            // Function to display the account
-            public void display_account()
-            {
-                Console.WriteLine("Name: " + name);
-                Console.WriteLine("Address: " + address);
-                Console.WriteLine("Type: " + acc_type);
-                Console.WriteLine("Balance: " + balance + "\n");
+                data = d;
+                next = null;
             }
         }
-        public static void Main(string[] args)
+
+        /* function to insert a new_node in a list. */
+        void sortedInsert(Node new_node)
         {
-            int choice;
-            // Creating Customer Object of Bank Class
-            Bank customer = new Bank();
+            Node current;
 
-            Console.WriteLine("\n1) Open account\n\n");
-            // Calling open_account() function through customer
-            // object.
-            customer.open_account();
-            Console.WriteLine("------------------------\n");
+            /* Special case for head node */
+            if (head == null || head.data >= new_node.data)
+            {
+                new_node.next = head;
+                head = new_node;
+            }
+            else
+            {
 
-            Console.WriteLine("\n2) Deposit account\n\n");
-            // Calling deposit_money() function through customer
-            // object.
-            customer.deposit_money();
-            Console.WriteLine("------------------------\n");
+                /* Locate the node before 
+                point of insertion. */
+                current = head;
 
-            Console.WriteLine("\n2) Withdraw money\n\n");
-            // Calling withdraw_money() function through
-            // customer object.
-            customer.withdraw_money();
-            Console.WriteLine("------------------------\n");
+                while (current.next != null && current.next.data < new_node.data)
+                    current = current.next;
 
-            Console.WriteLine("\n4) Display Account\n\n");
-            // Calling display_account() function through
-            // customer object.
-            customer.display_account();
-            Console.WriteLine("------------------------\n");
+                new_node.next = current.next;
+                current.next = new_node;
+            }
+        }
 
-            Console.ReadKey();
+        /*Utility functions*/
+
+        /* Function to create a node */
+        Node newNode(int data)
+        {
+            Node x = new Node(data);
+            return x;
+        }
+
+        /* Function to print linked list */
+        void printList()
+        {
+            Node temp = head;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+
+        /* Driver code */
+        public static void Main(String[] args)
+        {
+            LinkedList llist = new LinkedList();
+            Node new_node;
+
+            new_node = llist.newNode(5);
+            llist.sortedInsert(new_node);
+
+            new_node = llist.newNode(10);
+            llist.sortedInsert(new_node);
+
+            new_node = llist.newNode(7);
+            llist.sortedInsert(new_node);
+
+            new_node = llist.newNode(3);
+            llist.sortedInsert(new_node);
+
+            new_node = llist.newNode(1);
+            llist.sortedInsert(new_node);
+
+            new_node = llist.newNode(9);
+            llist.sortedInsert(new_node);
+
+            Console.WriteLine("Created Linked List");
+            llist.printList();
         }
     }
 }
